c9c45bcbfe7f889e3604955c117139205d3613dd
olga-kacala <olga.kacala@gmail.com>
2023-11-14 20:03:28 +0100

    contact

--- my-ts-react-app/src/components/Contact/Contact.tsx
+++ my-ts-react-app/src/components/Contact/Contact.tsx
@@ -10,30 +10,39 @@ export function Contact() {
           alt="HydroSeb cars"
           src={"../../IMG/kontakt.jpg"}
         />
-
         <div className={classes.homeText}>
           <div className={classes.bodyTitle}>
-            POGOTOWIE KANALIZACYJNE 24H/7DNI
-          </div>
-          <div className={classes.bodyTitle}>tel. 502 220 475</div>
-          <div className={classes.smallbodyTitle}>
-            CZĘSTOCHOWA I OKOLICE DO 60KM - DOJAZD GRATIS
-          </div>
-          <div className={classes.bodyTitle}>
-            Dojazd hydraulika nawet w 15 minut*
+            F.U. HYDRO SEB - CZYSZCZENIE, UDRAŻNIANIE INSPEKCJA KANALIZACJI
+            <br />
+            SROCKO 64
+            <br />
+            42-244 MSTÓW
           </div>
         </div>
       </div>
       <div className={classes.mainFeedContainer}>
         <div className={classes.textContainer}>
-          F.U. HYDRO SEB - CZYSZCZENIE, UDRAŻNIANIE INSPEKCJA KANALIZACJI.
-          SROCKO 64 42-244 MSTÓW
-        </div>
+          <h2>
+            Dzięki dobrej lokalizacji siedziby firmy jesteśmy w stanie
+            błyskawicznie przyjechać do klienta omijając korki.
+          </h2>
 
-        <div className={classes.ContactContainer}>
-          Zapchała Ci się rura kanalizacyjna? Często powstają zatory w rurach?
-          Nie zwlekaj. Dzwoń po hydraulika. Już podczas rozmowy jesteśmy w
-          stanie wycenić usługę.
+          <div className={classes.mapContainer}>
+              <img
+                title="map"
+                alt="map"
+                src={"../../IMG/map.jpg"}
+                className={classes.mapImage}
+              />
+             
+              <div className={`${classes.marker} ${classes.marker1}`} />
+              <div className={`${classes.marker} ${classes.marker2}`} />
+              <div className={`${classes.marker} ${classes.marker3}`} />
+              <div className={`${classes.marker} ${classes.marker4}`} />
+            </div>
+
+
+        
         </div>
       </div>
     </div>
